8dd943a45a23b83f1679144125b8395e5cd0674b
Saurabh Singh <saurabhsunilsingh@gmail.com>
2024-02-12 18:39:31 +0530

    Added images and animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,10 @@ export default function Hero() {
             <p className={styles.description}>I am a developer with 3 years of experience using React, Unqork and NodeJS</p>
             <a className={styles.contactBtn} href="mailto:saurabhsunilsingh@gmail.com">Contact Me</a>
         </div>
+        <div className={styles.imageContainer}>
         <img className={styles.heroImage} src={getImageurl("hero/heroImage.png")} alt="hero-image" />
+        <img className={styles.bottomImage} src={getImageurl("hero/samuraisingh.png")} alt="jack-image" />
+        </div>
         <div className={StyleSheet.topBlur}></div>
         <div className={StyleSheet.bottomBlur}></div>
     </section>
